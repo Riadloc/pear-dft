c7ff4c721c3ae3c19b059bf00d6729414ea3e2a0
ywang <jaegerwang@didiglobal.com>
2022-04-29 01:19:43 +0800

    feat: 公众号

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -3,14 +3,14 @@
     <van-nav-bar :border="false" title="联系我们" fixed left-arrow @click-left="back" />
     <van-image
       width="80vw"
-      src="/qq-group.jpeg"
+      src="/mp.png"
     >
       <template v-slot:loading>
         <van-loading type="spinner" size="20" />
       </template>
     </van-image>
-    <h3 class="mt-4 text-white font-medium text-lg">加入官方群了解更多资讯</h3>
-    <a class="mt-2 text-green-500 text-sm underline" href="/qq-group.jpeg" download="梨数藏官方QQ群.jpeg">保存至手机</a>
+    <h3 class="mt-4 text-white font-medium text-lg">关注公众号了解更多咨询</h3>
+    <a class="mt-2 text-green-500 text-sm underline" href="/mp.png" download="梨数藏公众号.png">保存至手机</a>
   </div>
 </template>
 
