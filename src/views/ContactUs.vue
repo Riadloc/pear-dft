8c09d10ca01f706b03a391e25a11f344d7828153
ywang <jaegerwang@didiglobal.com>
2022-04-29 01:35:12 +0800

    fix: 联系我们

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -3,14 +3,14 @@
     <van-nav-bar :border="false" title="联系我们" fixed left-arrow @click-left="back" />
     <van-image
       width="80vw"
-      src="/mp.png"
+      src="/mp.jpg"
     >
       <template v-slot:loading>
         <van-loading type="spinner" size="20" />
       </template>
     </van-image>
     <h3 class="mt-4 text-white font-medium text-lg">关注公众号了解更多咨询</h3>
-    <a class="mt-2 text-green-500 text-sm underline" href="/mp.png" download="梨数藏公众号.png">保存至手机</a>
+    <a class="mt-2 text-green-500 text-sm underline" href="/mp.jpg" download="梨数藏公众号.jpg">保存至手机</a>
   </div>
 </template>
 
