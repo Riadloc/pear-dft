0c330e00155f096abf06fdb99058dbfa638c4035
young <wanggle1996@qq.com>
2022-06-14 12:16:57 +0800

    chore: env环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import styleImport, { VantResolve } from 'vite-plugin-style-import'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({ mode }) => defineConfig({
   plugins: [
     vue(),
     styleImport({
@@ -15,7 +15,7 @@ export default defineConfig({
     createHtmlPlugin({
       inject: {
         data: {
-          title: import.meta.env.VITE_DOMAIN_NAME
+          title: loadEnv(mode, process.cwd()).VITE_DOMAIN_NAME
         }
       }
     })
