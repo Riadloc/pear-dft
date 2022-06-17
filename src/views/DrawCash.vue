9ee677e03874eb935263c22be945bd3b55401810
young <wanggle1996@qq.com>
2022-06-17 22:06:45 +0800

    fix

--- src/views/DrawCash.vue
+++ src/views/DrawCash.vue
@@ -246,10 +246,9 @@ export default defineComponent({
         Toast('请获取验证码')
         return
       }
-      const { txn_seqno: seqNo, orderNo, total_amount: price, token } = dcData.value.data
+      const { txn_seqno: orderNo, total_amount: price, token } = dcData.value.data
       runCheckSms({
         orderNo,
-        txnOrderNo: seqNo,
         price,
         token,
         code: formData.code
